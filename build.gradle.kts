8b4e484cfb8df399c48411b2e7eb70fad66af255
David Mak <david.18.19.21@gmail.com>
2018-11-17 22:54:45 +0800

    [General] Remove redundant plugins in Gradle
    We are now using Java 8! No more OpenJFX dependencies and whatnot!

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,6 @@ repositories {
 plugins {
     application
     java
-    id("com.google.osdetector") version "1.6.0"
-    id("org.jetbrains.intellij") version "0.3.12"
 }
 
 application {
@@ -16,33 +14,14 @@ application {
 }
 
 java {
-    // TODO(Derppening): Test with Java 10 before submitting!
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
 }
-
-intellij {
-    instrumentCode = true
-}
-
-val platform: String = when (osdetector.os) {
-    "osx" -> "mac"
-    "windows" -> "win"
-    else -> osdetector.os
-}
-
-val javaHome = System.getenv()["JAVA_HOME"] ?: ""
 
 dependencies {
     val junit = "5.3.1"
     val junitPlatform = "1.3.1"
     val testfx = "4.0.15-alpha"
-
-    // TODO(Derppening): Uncomment these when testing with Java 10
-//    compile("org.openjfx:javafx-base:11:$platform")
-//    compile("org.openjfx:javafx-controls:11:$platform")
-//    compile("org.openjfx:javafx-graphics:11:$platform")
-//    compile("org.openjfx:javafx-media:11:$platform")
 
     compile("org.jetbrains:annotations:16.0.3")
 
@@ -64,22 +43,6 @@ sourceSets {
 }
 
 tasks {
-    getByName<JavaCompile>("compileJava") {
-        // TODO(Derppening): Uncomment these when testing with Java 10
-        doFirst {
-//            options.compilerArgs.addAll(listOf(
-//                    "--module-path", classpath.asPath,
-//                    "--add-modules", "javafx.controls,javafx.graphics,javafx.media"))
-        }
-    }
-
-    getByName<JavaExec>("run") {
-        // TODO(Derppening): Uncomment these when testing with Java 10
-//        jvmArgs?.addAll(listOf(
-//                "--module-path", classpath.asPath,
-//                "--add-modules", "javafx.controls,javafx.graphics,javafx.media"))
-    }
-
     getByName<Test>("test") {
         useJUnitPlatform()
     }
